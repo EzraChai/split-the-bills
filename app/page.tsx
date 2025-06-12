198353fc58f85754385ecabcb3e78f12d5c3b787
Ezra Chai <juanzhe2@gmail.com>
2025-06-13 00:05:46 +0800

    fix: update header text to enhance clarity and engagement

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="grid lg:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl lg:text-4xl font-bold text-center sm:text-left">
-          How much do I have to pay?
+        <h1 className="text-2xl lg:text-4xl  text-center sm:text-left">
+          Let's split the bill <span className="italic font-black">EVENLY</span>
+          .
         </h1>
         <PaidForm />
       </main>
